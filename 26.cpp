8a628dbe0b008679a3ceed66e2d8c889ebd7d8c4
Tsuwagi <viktoria16kutas5@gmail.com>
2025-05-18 23:49:52 +0300

    Update 26.cpp

--- 26.cpp
+++ 26.cpp
@@ -8,10 +8,10 @@ bool first_last_letter(char* line) {
     int len = strlen(line);
 
     if (len > 0 && line[len - 1] == '\n') 
-    len--;
+        len--;
 
     if (len == 0) 
-    return false;
+        return false;
 
     int first = 0;
     int last = len - 1;
@@ -19,15 +19,15 @@ bool first_last_letter(char* line) {
     return line[first] == line[last];
 }
 
-int main() {
+int View_file(const char* filename) {
     FILE* f;
     char s[100];
     int count = 0;
 
-    f = fopen("myfile.txt", "rt");
+    f = fopen(filename, "rt");
     if (f == NULL) {
         cout << "Неможливо відкрити файл\n";
-        return 1;
+        return -1;
     }
 
     while (fgets(s, 100, f)) {
@@ -37,6 +37,14 @@ int main() {
     }
 
     fclose(f);
+    return count;
+}
+
+int main() {
+    const char* filename = "myfile.txt";
+    int count = View_file(filename);
+
     cout << "Кількість рядків: " << count << endl;
+
     return 0;
 }
